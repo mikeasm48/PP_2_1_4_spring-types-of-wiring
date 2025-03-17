5830bdedabcc479694ba0026ccd0da8da3357df6
mikeasm <mikeasm48@gmail.com>
2025-03-17 13:36:19 +0500

    TASK214 - решение

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -2,8 +2,13 @@ package koschei.models;
 
 public class Egg6 {
 
+    Needle7 needle7;
+    public Egg6(Needle7 needle) {
+        this.needle7 = needle;
+    }
+
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle7.toString();
     }
 }
